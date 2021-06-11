b3df3d08ad00bd2df69b6e43ffbe5f4c1936674f
Ethan Ligon <ligon@berkeley.edu>
2021-06-11 11:59:04 -0700

    Fix syntax error in setup.py.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email='ligon@berkeley.edu',
     packages=['metrics_miscellany',],
     license='Creative Commons Attribution-Noncommercial-ShareAlike 4.0 International license',
-    description='Miscellaneous code for estimation involving pandas dataframes.",
+    description='Miscellaneous code for estimation involving pandas dataframes.',
     url='https://bitbucket.org/ligonresearch/metrics_miscellany',
     long_description=open('README.txt').read(),
     setup_requires = ['pytest_runner'],
