6b092650982167336f07a3edd651a291b4f402fe
Ethan Ligon <ligon@berkeley.edu>
2025-05-19 15:17:28 -0700

    Add factor analysis (with missing data); some matrix methods.
    Also: kernel regression.

--- metrics_miscellany/__init__.py
+++ metrics_miscellany/__init__.py
@@ -3,3 +3,4 @@ from . import tests
 from . import utils
 from . import order_statistics
 from .datamat import DataMat, DataVec
+from . import kernel_methods
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     license='Creative Commons Attribution-Noncommercial-ShareAlike 4.0 International license',
     description='Miscellaneous code for estimation involving pandas dataframes.',
     url='https://bitbucket.org/ligonresearch/metrics_miscellany',
-    long_description=open('README.txt').read(),
+    long_description=open('README.org').read(),
     setup_requires = ['pytest_runner'],
     tests_require = ['pytest']
 )
