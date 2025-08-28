f4a7d30e02ea4ec1792c444bb7b09c5e9e505010
Ethan Ligon <ligon@berkeley.edu>
2025-08-28 16:39:55 -0700

    Complete FWL suite; fix errors; bump version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 
 setup(
     name='Metrics_Miscellany',
-    version='0.2.4',
+    version='0.2.5',
     author='Ethan Ligon',
     author_email='ligon@berkeley.edu',
     packages=['metrics_miscellany',],
